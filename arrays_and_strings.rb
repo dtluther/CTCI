f272674b67b30f9d56f7b7c5d34637341bd4c997
Dillon <dillontluther@gmail.com>
2017-11-30 18:35:09 -0800

    palindrome permutation

--- arrays_and_strings.rb
+++ arrays_and_strings.rb
@@ -20,7 +20,7 @@ end
 # p is_unique?("123/.,abc")
 # p is_unique?("123/.,..")
 
-def check_permutations(str1, str2)
+def check_permutations?(str1, str2)
     return false if str1.length != str2.length
 
     count1 = Hash.new(0)
@@ -41,6 +41,31 @@ end
     # 2a because will return false if they aren't the same length
 # O(2a) space complexity because it will make 2 maps of a length
 
-# p check_permutations("eat", "ate")
-# p check_permutations("nope", "nice try")
-# p check_permutations("12345", "54321")
+# p check_permutations?("eat", "ate")
+# p check_permutations?("nope", "nice try")
+# p check_permutations?("12345", "54321")
+
+def palindrome_permutation?(str)
+    counts = Hash.new(0)
+    odd_count = 0
+
+    str.each_char do |ch|
+        next if ch == " "
+        counts[ch] += 1
+    end
+
+    counts.values.each do |n|
+        odd_count += 1 if n .odd?
+        return false if odd_count > 1
+    end
+
+    true
+end
+# O(2n) => O(n) time complexity because two separate iterations
+# O(n + 1) => O(n) space complexity because of the n-length map and single count
+
+# p palindrome_permutation?("tact coa") # "taco cat"
+# p palindrome_permutation?("a toyota") # "a toyota"
+# p palindrome_permutation?("car race") # "racecar"
+# p palindrome_permutation?("14145252336") # "12345654321"
+# p palindrome_permutation?("12345")
