31ea5b9c56b50a92d853bc057f9553a89f797bdc
Dillon <dillontluther@gmail.com>
2018-01-02 16:40:04 -0800

    working on DFS

--- trees_and_graphs/trees_and_graphs.rb
+++ trees_and_graphs/trees_and_graphs.rb
@@ -1,9 +1,12 @@
-class Node
-    attr_reader :name, :children
+require 'byebug'
 
-    def initialize(name, children_array)
+class Node
+    attr_reader :name, :neighbors
+
+    def initialize(name, neighbors_array)
         @name = name
-        @children = children_array
+        @neighbors = neighbors_array
+        @seen = false
     end
 end
 
@@ -15,14 +18,35 @@ class Graph
     end
 end
 
-node5 = Node.new("node1", [])
-node4 = Node.new("node2", [node5])
-node3 = Node.new("node1", [node4])
-node2 = Node.new("node1", [node3])
+node6 = Node.new("node5", [])
+node5 = Node.new("node5", [])
+node4 = Node.new("node4", [node5])
+node3 = Node.new("node3", [node4])
+node2 = Node.new("node2", [node3, node6])
 node1 = Node.new("node1", [node2, node3])
 
 graph = Graph.new([node1, node2, node3, node4, node5])
 
-def routes_between_nodes(graph)
-    
+def routes_between_nodes(source, terminal) # source is a term for start node, terminal for end
+    # # Naive solution would be to iterate through each of the neighbor's neighbors
+    # # of each node until the other node is found or there are no further nodes left
+    # # in the neighbors's neighbors
+    #
+    # if source.neighbors.length > 0
+    #     source.neighbors.each do |node|
+    #         # debugger
+    #         node
+    #         return true if node == terminal
+    #         return routes_between_nodes(node, terminal)
+    #     end
+    # end
+    #
+    # false
+
+    # BFS Implementation
+    queue = []
+
 end
+
+p routes_between_nodes(node1, node5)
+p routes_between_nodes(node3, node6)
